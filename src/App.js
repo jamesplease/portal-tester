bcab0e01d640fc960f0b123ec1eb53f26364ef37
Jmeas <jellyes2@gmail.com>
2018-05-19 12:47:46 -0700

    Add support for positioning

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, createRef } from "react";
 import "./App.css";
 import Tooltip from "./tooltip";
 import createGlobalComponent from "./create-global-component";
@@ -7,18 +7,26 @@ const GlobalTooltip = createGlobalComponent();
 
 class App extends Component {
   render() {
-    const { tooltipOpen } = this.state;
+    const { tooltipOpen, boundingBox } = this.state;
 
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Global Component Test Thing</h1>
         </header>
-        <button onClick={() => this.setState({ tooltipOpen: !tooltipOpen })}>
+        <button ref={this.buttonRef} onClick={this.onClickBtn}>
           Open Popover
         </button>
-        <GlobalTooltip active={tooltipOpen} timeout={1000} classNames="fade">
-          <Tooltip />
+        <GlobalTooltip
+          active={tooltipOpen}
+          timeout={1000}
+          classNames="fade"
+          boundingBox={boundingBox}
+        >
+          {data => {
+            console.log("got the data:", data);
+            return <Tooltip />;
+          }}
         </GlobalTooltip>
       </div>
     );
@@ -26,6 +34,29 @@ class App extends Component {
 
   state = {
     tooltipOpen: false
+  };
+
+  buttonRef = createRef();
+
+  onClickBtn = () => {
+    const { tooltipOpen } = this.state;
+
+    if (!this.buttonRef.current) {
+      return;
+    }
+
+    const boundingBox = this.buttonRef.current.getBoundingClientRect();
+
+    const newState = {
+      tooltipOpen: !tooltipOpen,
+      boundingBox: {
+        left: boundingBox.left,
+        top: boundingBox.top,
+        width: boundingBox.width
+      }
+    };
+
+    this.setState(newState);
   };
 }
 
--- src/create-global-component.js
+++ src/create-global-component.js
@@ -2,18 +2,25 @@ import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import { CSSTransition } from "react-transition-group";
 
+function computeDimensionsFromBoundingBox(boundingBox) {}
+
 export default function createGlobalComponent(rootNode = document.body) {
   return class GlobalComponent extends Component {
     render() {
-      const { children, active, timeout, classNames } = this.props;
+      const { children, active, timeout, classNames, boundingBox } = this.props;
+      const { dimensions } = this.state;
 
       return ReactDOM.createPortal(
         <CSSTransition in={active} timeout={timeout} classNames={classNames}>
-          {children}
+          {children({ dimensions })}
         </CSSTransition>,
         this.el
       );
     }
+
+    state = {
+      dimensions: {}
+    };
 
     componentDidMount() {
       rootNode.appendChild(this.el);
@@ -21,6 +28,21 @@ export default function createGlobalComponent(rootNode = document.body) {
 
     componentWillUnmount() {
       rootNode.removeChild(this.el);
+    }
+
+    static getDerivedStateFromProps(nextProps, prevState) {
+      const { boundingBox } = nextProps;
+
+      if (!boundingBox) {
+        return {};
+      }
+
+      return {
+        dimensions: {
+          whatever: boundingBox.width * 2,
+          whateverTwo: boundingBox.left * 5
+        }
+      };
     }
 
     el = document.createElement("div");
